6e7eee8738132cc2c6691982e9377abed42be93b
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-14 18:57:52 -0400

    Refactor: Byte argument

--- src/main/kotlin/com/lambda/interfaces/SocketEvent.kt
+++ src/main/kotlin/com/lambda/interfaces/SocketEvent.kt
@@ -3,9 +3,7 @@ package com.lambda.interfaces
 import java.io.BufferedWriter
 
 interface SocketEvent {
-    fun receive(bit: List<String>, vararg args: Array<String>)
+    fun receive(byte: Byte, vararg args: Array<String>)
     fun close(): Boolean
     fun send(data: String, bw: BufferedWriter)
-    fun String.encode(): String
-    fun String.decode(): String
 }
