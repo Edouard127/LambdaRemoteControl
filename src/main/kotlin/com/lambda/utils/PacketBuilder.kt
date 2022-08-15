14510229dfe825b141f383826ff4207d7706ee8e
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-14 23:28:08 -0400

    PacketBuilder -> PacketData

--- src/main/kotlin/com/lambda/utils/PacketBuilder.kt
+++ src/main/kotlin/com/lambda/utils/PacketBuilder.kt
@@ -1,15 +1,6 @@
 package com.lambda.utils
 
-import com.lambda.client.event.SafeClientEvent
-import com.lambda.enums.EPacket
 import com.lambda.interfaces.IPacketBuilder
-import com.lambda.modules.RemoteControl
-import com.lambda.utils.Worker
-import net.minecraft.client.Minecraft
-import org.apache.commons.codec.binary.Hex
-import java.util.*
-import javax.crypto.Mac
-import javax.crypto.spec.SecretKeySpec
 
 class PacketBuilder(val byte: Byte, val data: ByteArray) : IPacketBuilder {
     override fun buildPacket(): Packet {
@@ -17,29 +8,6 @@ class PacketBuilder(val byte: Byte, val data: ByteArray) : IPacketBuilder {
     }
 
 }
-
-val defaultHeartbeat = byteArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
-val defaultLogin = byteArrayOf("2b2t.com.de".toByte(), "25565".toByte())
-val defaultLogout = byteArrayOf("Client received logout packet".toByte())
-val defaultGetWorkers = byteArrayOf() //TODO: Implement this
-val defaultGetWorkersStatus = byteArrayOf() //TODO: Implement this
-val defaultChat = byteArrayOf("Hello World".toByte())
-val defaultBaritone = byteArrayOf("help".toByte())
-val defaultLambda = byteArrayOf("help".toByte())
-val defaultError = byteArrayOf("Client error".toByte())
-
-fun getWorker(): ByteArray {
-    val mc = Minecraft.getMinecraft()
-    return byteArrayOf(mc.player.name.toByte(), createSignature(mc.player.name, RemoteControl.secretKey))
-}
-fun createSignature(data: String, key: String): Byte {
-    val sha256Hmac = Mac.getInstance("HmacSHA256")
-    val secretKey = SecretKeySpec(key.toByteArray(), "HmacSHA256")
-    sha256Hmac.init(secretKey)
-
-    return Hex.encodeHexString(sha256Hmac.doFinal(data.toByteArray())).toByte()
-}
-
 /*fun getWorkerStatus(worker: IWorker): Byte {
     return worker.status.byte
 }*/
--- src/main/kotlin/com/lambda/utils/PacketData.kt
+++ src/main/kotlin/com/lambda/utils/PacketData.kt
@@ -2,6 +2,11 @@ package com.lambda.utils
 
 import com.lambda.enums.EPacket
 import com.lambda.interfaces.IPacketData
+import com.lambda.modules.RemoteControl
+import net.minecraft.client.Minecraft
+import org.apache.commons.codec.binary.Hex
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 class PacketData(val packet: EPacket) : IPacketData {
     override fun defaultData(): ByteArray {
@@ -21,4 +26,29 @@ class PacketData(val packet: EPacket) : IPacketData {
             EPacket.ERROR -> defaultError
         }
     }
+}
+
+val defaultHeartbeat = byteArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+val defaultLogin = "2b2t.com.de 25565".encodeToByteArray()
+val defaultLogout = "Client received logout packet".encodeToByteArray()
+val defaultGetWorkers = byteArrayOf() //TODO: Implement this
+val defaultGetWorkersStatus = byteArrayOf() //TODO: Implement this
+val defaultChat = "Hello World".encodeToByteArray()
+val defaultBaritone = "help".encodeToByteArray()
+val defaultLambda = "help".encodeToByteArray()
+val defaultError = "Client error".encodeToByteArray()
+
+
+fun getWorker(): ArrayList<ByteArray> {
+    val data = ArrayList<ByteArray>()
+    data.add("Kamigen".encodeToByteArray())
+    data.add(createSignature("Kamigen", "Kamigen"))
+    return data
+}
+fun createSignature(data: String, key: String): ByteArray {
+    val sha256Hmac = Mac.getInstance("HmacSHA256")
+    val secretKey = SecretKeySpec(key.toByteArray(), "HmacSHA256")
+    sha256Hmac.init(secretKey)
+
+    return Hex.encodeHexString(sha256Hmac.doFinal(data.toByteArray())).encodeToByteArray()
 }
