e682e35cdd229689e286f02eefdd8faaef24366e
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-15 12:03:27 -0400

    Create PacketUtils.kt

---
+++ src/main/kotlin/com/lambda/utils/PacketUtils.kt
@@ -0,0 +1,27 @@
+package com.lambda.utils
+
+import com.lambda.enums.EPacket
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
+object PacketUtils {
+    fun <T> encode(packet: T): ByteArray {
+        val baos = ByteArrayOutputStream()
+        val oos = ObjectOutputStream(baos)
+        oos.writeObject(packet)
+        oos.close()
+        return baos.toByteArray()
+    }
+    fun <T> decode(data: ByteArray): T {
+        val bais = ByteArrayInputStream(data)
+        val ois = ObjectInputStream(bais)
+        val packet = ois.readObject() as T
+        ois.close()
+        return packet
+    }
+    fun getPacketId(data: Byte): EPacket {
+        return EPacket.values()[data.toInt()]
+    }
+}
