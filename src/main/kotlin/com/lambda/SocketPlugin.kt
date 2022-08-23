eb9f8e035292a0cd8453f203fea130709392f644
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-08-23 15:11:30 -0400

    Debug

--- src/main/kotlin/com/lambda/SocketPlugin.kt
+++ src/main/kotlin/com/lambda/SocketPlugin.kt
@@ -2,10 +2,12 @@ package com.lambda
 
 import com.lambda.client.plugin.api.Plugin
 import com.lambda.modules.RemoteControl
+import com.lambda.utils.Debug
 
 internal object SocketPlugin : Plugin() {
 
     override fun onLoad() {
+        Debug.purple("SocketPlugin loaded")
         modules.add(RemoteControl)
     }
 
